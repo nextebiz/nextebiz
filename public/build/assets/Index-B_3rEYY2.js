5dc172f452c3809e9d9fae65667ef41424a390a2
fullstackimran <fullstackimran@gmail.com>
2024-04-29 16:28:06 +0500

    build 8

---
+++ public/build/assets/Index-B_3rEYY2.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./AuthenticatedLayout-B1oMHJvZ.js";import{o as t,i as o,b as s,c as n,w as r,d as a}from"./app-CNx8M9mI.js";import"./SettingsStore-C4EshwRf.js";const c=a("div",{class:"container m-auto"},[a("h1",null,"Trainee Program")],-1),p={__name:"Index",setup(i){return t(()=>{o()}),(_,l)=>(s(),n(e,null,{default:r(()=>[c]),_:1}))}};export{p as default};
--- public/build/assets/Index-DpWSlpTF.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./AuthenticatedLayout-2fu2IMFq.js";import{o as a,i as e,b as s,c as n,w as r,d as o}from"./app-CaOeAmca.js";import"./SettingsStore-BoLsPae9.js";const c=o("div",{class:"container m-auto"},[o("h1",null,"Portfolio")],-1),p={__name:"Index",setup(i){return a(()=>{e()}),(_,l)=>(s(),n(t,null,{default:r(()=>[c]),_:1}))}};export{p as default};
