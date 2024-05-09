ab778403ade4435a601d673c3a8e2ef040076e22
fullstackimran <fullstackimran@gmail.com>
2024-05-10 02:42:47 +0500

    fix2

--- public/build/assets/AdminLayout-S4fa9br3.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./AuthenticatedLayout-B7vuKxth.js";import e from"./AdminMenu-DV060ZKa.js";import{o as a,b as r,c as s,w as n,r as _,e as i}from"./app-jdL9wzWZ.js";import"./SettingsStore-DFwRfRy1.js";const d={__name:"AdminLayout",setup(m){return a(()=>{initFlowbite()}),(t,c)=>(r(),s(o,null,{default:n(()=>[_(t.$slots,"default"),i(e)]),_:3}))}};export{d as default};
--- public/build/assets/Index-DDbrBATL.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./AuthenticatedLayout-B7vuKxth.js";import{o as t,i as o,b as s,c as n,w as r,d as a}from"./app-jdL9wzWZ.js";import"./SettingsStore-DFwRfRy1.js";const c=a("div",{class:"container m-auto"},[a("h1",null,"Trainee Program")],-1),p={__name:"Index",setup(i){return t(()=>{o()}),(_,l)=>(s(),n(e,null,{default:r(()=>[c]),_:1}))}};export{p as default};
