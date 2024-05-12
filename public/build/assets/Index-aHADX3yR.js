0f4455d1d78b94fb893d60fd41f8b058422610d5
fullstackimran <fullstackimran@gmail.com>
2024-05-12 21:25:12 +0500

    layout fix

--- public/build/assets/Index-aHADX3yR.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./AuthenticatedLayout-BndGssem.js";import{o as e,i as t,a as n,b as s,c,w as r,d as o}from"./app-BIo4__G-.js";import"./SettingsStore-BX5ss7eW.js";const _=o("div",{class:"container m-auto"},[o("h1",null,"Aboutx")],-1),f={__name:"Index",setup(i){return e(()=>{t()}),n(()=>{t()}),(u,l)=>(s(),c(a,null,{default:r(()=>[_]),_:1}))}};export{f as default};
--- resources/js/Pages/Expertise/Show.vue
+++ resources/js/Pages/Expertise/Show.vue
@@ -91,21 +91,24 @@ onMounted(() => {
                     </div>
                     <div class=" w-full p-0 mb-5">
                         <button @click="changeSelectedCategory(jobcategory.id)" data-modal-target="crud-modal" data-modal-toggle="crud-modal" class="w-full" type="button">
-                            <div class="flex items-center justify-center py-3 text-xl font-bold rounded-lg text-white  bg-orange-600 hover:bg-orange-500 hover:border-orange-800   transition-all">
-                                <span class="text-white ">
-                                    Hire {{ jobcategory.title }}
-                                </span>
-                                <div class="w-[30px] mx-5 fill-white">
+                            <div class="block sm:flex items-center justify-center py-3 text-xl font-bold rounded-lg text-white  bg-orange-600 hover:bg-orange-500 hover:border-orange-800   transition-all">
+                                <div>
+                                    <span class="text-white text-lg font-normal md:text-2xl md:font-bold px-2 md:px-0">
+                                        Hire {{ jobcategory.title }}
+                                    </span>
+                                </div>
+                                <div class="w-[30px] mx-5 fill-white hidden md:inline-block">
                                     <svg xmlns="http://www.w3.org/2000/svg"
                                         viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                                         <path
                                             d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM294.6 135.1c-4.2-4.5-10.1-7.1-16.3-7.1C266 128 256 138 256 150.3V208H160c-17.7 0-32 14.3-32 32v32c0 17.7 14.3 32 32 32h96v57.7c0 12.3 10 22.3 22.3 22.3c6.2 0 12.1-2.6 16.3-7.1l99.9-107.1c3.5-3.8 5.5-8.7 5.5-13.8s-2-10.1-5.5-13.8L294.6 135.1z" />
                                     </svg>
                                 </div>
-                                <span>
-                                    Starting from ${{ jobcategory.min }} / Hour
-                                </span>
-
+                                <div>
+                                    <span>
+                                        Starting from ${{ jobcategory.min }} / Hour
+                                    </span>
+                                </div>
                             </div>
                         </button>
                     </div>
