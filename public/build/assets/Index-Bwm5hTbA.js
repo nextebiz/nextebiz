a5a73911b781ca88111fa9597de39b65265ab96c
fullstackimran <fullstackimran@gmail.com>
2024-05-12 18:54:28 +0500

    footer fix

--- public/build/assets/Index-Bwm5hTbA.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./AuthenticatedLayout-DwL47QPN.js";import{o as t,i as o,b as s,c as n,w as r,d as a}from"./app-B4W5m-19.js";import"./SettingsStore-B1mJMi5f.js";const c=a("div",{class:"container m-auto"},[a("h1",null,"Trainee Program")],-1),p={__name:"Index",setup(i){return t(()=>{o()}),(_,l)=>(s(),n(e,null,{default:r(()=>[c]),_:1}))}};export{p as default};
---
+++ public/build/assets/Index-HOuy9k6o.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./AuthenticatedLayout-De2UmtDg.js";import{o as e,i as t,a as n,b as s,c,w as r,d as o}from"./app-C9vjdT2e.js";import"./SettingsStore-BtfZPAbS.js";const _=o("div",{class:"container m-auto"},[o("h1",null,"Aboutx")],-1),f={__name:"Index",setup(i){return e(()=>{t()}),n(()=>{t()}),(u,l)=>(s(),c(a,null,{default:r(()=>[_]),_:1}))}};export{f as default};
